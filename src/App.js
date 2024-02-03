fbc0c075e254e7e3192a6161dc0ada8f94372eb8
Saurabh Kumar Tiwari <sky.royaltiwari@gmail.com>
2024-02-03 17:42:22 +0530

    done-working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import ConvertData from './Converter';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
-
       <ConvertData />
     </div>
   );
--- src/Converter.js
+++ src/Converter.js
@@ -7,7 +7,7 @@ const ConvertData = () => {
   const [toCurrency, setToCurrency] = useState('INR');
   const [conversionRate, setConversionRate] = useState(null);
   const [currencies, setCurrencies] = useState([]);
-  
+
 
   useEffect(() => {
     fetch('https://currency-exchange.p.rapidapi.com/listquotes', {
@@ -18,7 +18,7 @@ const ConvertData = () => {
     })
       .then(response => response.json())
       .then(data => {
-        console.log(data);
+        // console.log(data);
         setCurrencies(data);
       })
       .catch(error => {
@@ -27,6 +27,10 @@ const ConvertData = () => {
   }, []);
 
   useEffect(() => {
+    if (fromCurrency === toCurrency) {
+      alert('Source and target currencies cannot be the same!');
+      return;
+    }
     fetch(`https://currency-exchange.p.rapidapi.com/exchange?from=${fromCurrency}&to=${toCurrency}&q=${amount}`, {
       headers: {
         'X-RapidAPI-Host': 'currency-exchange.p.rapidapi.com',
@@ -54,33 +58,98 @@ const ConvertData = () => {
     setToCurrency(event.target.value);
   };
 
+  const handleToCountryCurrency = (data) => {
+    switch (data) {
+      case 'SGD':
+        return 'Singapore Dollars';
+      case 'MYR':
+        return 'Malaysian Ringgit';
+      case 'EUR':
+        return 'Euro';
+      case 'USD':
+        return 'US Dollars';
+      case 'AUD':
+        return 'Australian Dollars';
+      case 'JPY':
+        return 'Japanese Yen';
+      case 'CNH':
+        return 'Chinese Yuan';
+      case 'HKD':
+        return 'Hong Kong Dollars';
+      case 'CAD':
+        return 'Canadian Dollars';
+      case 'INR':
+        return 'Indian Rupees';
+      case 'DKK':
+        return 'Danish Krone';
+      case 'GBP':
+        return 'British Pounds';
+      case 'RUB':
+        return 'Russian Rubles';
+      case 'NZD':
+        return 'New Zealand Dollars';
+      case 'MXN':
+        return 'Mexican Pesos';
+      case 'IDR':
+        return 'Indonesian Rupiah';
+      case 'TWD':
+        return 'Taiwanese Dollar';
+      case 'THB':
+        return 'Thai Baht';
+      case 'VND':
+        return 'Vietnamese Dong';
+      default:
+        return '';
+    }
+  };
+
+
+  // const getFlagImageUrl = (countryCode) => {
+  //   return `/flags/${countryCode.toUpperCase()}.png`;
+  // };
+
+
   return (
-    <div className='container'>
-      <h1>Currency Converter</h1>
-      <div className='amount'>
-        <label>Amount:</label>
-        <input type="number" value={amount} onChange={handleAmountChange} />
-      </div>
-      <div>
-        <label>From Currency:</label>
-        <select value={fromCurrency} onChange={handleFromCurrencyChange}>
-          {currencies.map(currency => (
-            <option key={currency} value={currency}>{currency}</option>
-          ))}
-        </select>
-      </div>
-      <div>
-        <label>To Currency:</label>
-        <select value={toCurrency} onChange={handleToCurrencyChange}>
-          {currencies.map(currency => (
-            <option key={currency} value={currency}>{currency}</option>
-          ))}
-        </select>
-      </div>
-      <div className='summary'>
-      <h2>{amount} {fromCurrency} = {amount * conversionRate} {toCurrency}</h2>
-       <p>1 {fromCurrency} = {conversionRate} {  toCurrency}</p>
-        
+    <div className='main-container'>
+      <h2>{amount} {fromCurrency} to {toCurrency}  =  {handleToCountryCurrency(fromCurrency)} to {handleToCountryCurrency(toCurrency)} </h2>
+
+      <div className='container'>
+        <h1>Currency Converter</h1>
+
+        <div className='main'>
+          <div className='child-main'>
+            <label>Amount</label>
+            <input type="number" value={amount} onChange={handleAmountChange} />
+          </div>
+          <div className='child-main'>
+            <label>From</label>
+            <select value={fromCurrency} onChange={handleFromCurrencyChange}>
+              {currencies.map(currency => (
+                <option key={currency} value={currency}>
+                  {/* <span className={getFlagImageUrl(countryCodeMap[currency])}></span> */}
+                  {currency} - {handleToCountryCurrency(currency)}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className='child-main'>
+            <label>From</label>
+            <select value={toCurrency} onChange={handleToCurrencyChange}>
+              {currencies.map(currency => (
+                <option key={currency} value={currency}>
+                  {/* <span className={getFlagImageUrl(countryCodeMap[currency])}></span> */}
+                  {currency} - {handleToCountryCurrency(currency)}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+
+        <div className='summary'>
+          <h2>{amount} {fromCurrency} = {amount * conversionRate} {toCurrency}</h2>
+          <p>1 {fromCurrency} = {conversionRate} {toCurrency}</p>
+
+        </div>
       </div>
     </div>
   );
